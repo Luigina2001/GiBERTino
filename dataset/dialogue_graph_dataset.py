ce07acd0b2d90d0527a8ae04dc2bb1d4ba01436e
Luigina Costante <luiginacostante@gmail.com>
2025-02-18 21:04:32 +0100

    Implement negative sampling for link prediction, replacing full adjacency matrix

--- dataset/dialogue_graph_dataset.py
+++ dataset/dialogue_graph_dataset.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 import torch
 import numpy as np
 from utils import get_device
@@ -41,15 +43,43 @@ class DialogueGraphDataset(Dataset):
         relation_types = [edge_type[1] for edge_type in graph.edge_types if
                           edge_type[0] == edge_type[2] and edge_type[0] == 'edu']
 
-        link_labels, relation_labels = self._generate_labels(graph, relation_types)
+        # Collect positive edges
+        positive_edges = set()
+        relation_labels = []
+        for rel_type in relation_types:
+            edge_index = graph[rel_type].edge_index
+            for src, dst in edge_index.t():
+                if src < dst:  # removal of backward arches
+                    positive_edges.add((int(src), int(dst)))
+                    relation_labels.append(self._encode_relation_type(rel_type))
 
-        edges_permutations = list(permutations(torch.arange(graph["edu"].x.size(0)).tolist(), 2))
+
+        num_nodes = graph["edu"].x.size(0)
+        all_possible_edges = set(permutations(range(num_nodes), 2))
+        negative_edges = list(all_possible_edges - positive_edges)
+
+        # Sample a number of negatives edges
+        num_negatives = len(negative_edges)  # TODO: verificare numero di coppie negative
+        sampled_negative_edges = random.sample(negative_edges, num_negatives)
+
+        # Combine positive and negative pairs
+        candidate_edges = list(positive_edges) + sampled_negative_edges
+        candidate_edges_index = torch.tensor(candidate_edges, dtype=torch.long).T  # shape [2, num_candidate_edges]
+
+        # Labels: 1 for positive, 0 for negative
+        candidate_link_labels = torch.tensor([1] * len(positive_edges) + [0] * len(sampled_negative_edges),
+                                             dtype=torch.long)
+
+
+
+        # link_labels, relation_labels = self._generate_labels(graph, relation_types)
+        # edges_permutations = list(permutations(torch.arange(graph["edu"].x.size(0)).tolist(), 2))
 
         hetero_data = HeteroData()
         hetero_data["edu"].x = graph["edu"].x
-        hetero_data["edu", "to", "edu"].edge_index = torch.tensor(np.array(edges_permutations), dtype=torch.long).T
-        hetero_data["edu", "to", "edu"].link_labels = link_labels
-        hetero_data["edu", "to", "edu"].relation_labels = relation_labels
+        hetero_data["edu", "to", "edu"].edge_index = candidate_edges_index
+        hetero_data["edu", "to", "edu"].link_labels = candidate_link_labels
+        hetero_data["edu", "to", "edu"].relation_labels = torch.tensor(relation_labels, dtype=torch.long)
 
         return hetero_data
 
--- model/GiBERTino.py
+++ model/GiBERTino.py
@@ -21,12 +21,16 @@ class GiBERTino(pl.LightningModule):
         self.link_predictor = nn.Sequential(
             nn.Linear(hidden_channels * 2, hidden_channels),
             nn.ReLU(),
-            nn.Linear(hidden_channels, 1),
-            nn.LogSigmoid()
+            # nn.Linear(hidden_channels, 1),
+            # nn.LogSigmoid() # restituisce un singolo valore (probabilità logaritmica della classe 1)
+            nn.Linear(hidden_channels, 2),
+            nn.LogSoftmax(dim=1)
         )
 
-        self.link_pl = nn.NLLLoss()
-        self.rel_pl = nn.CrossEntropyLoss()
+
+        # NOTA: NLLLoss si aspetta due valori (probabilità logaritmiche sia per la classe 0 che per la classe 1).
+        self.link_pl = nn.NLLLoss()  # Binary classification loss for link prediction
+        self.rel_pl = nn.CrossEntropyLoss()  # Loss for relation classification
         self.metrics = Metrics(num_classes=NUM_RELATIONS)
 
     def forward(self, data):
@@ -39,27 +43,34 @@ class GiBERTino(pl.LightningModule):
         return link_probs
 
     def _predict_links(self, node_embeddings, edge_index):
-        adj_matrix = torch.zeros(node_embeddings.shape[0], node_embeddings.shape[0])
-        print(adj_matrix)
-        mask = ~torch.eye(node_embeddings.shape[0], dtype=torch.bool)
-        for i in range(edge_index.shape[1]):
-            src = edge_index[0][i]
-            dst = edge_index[1][i]
-            src_emb = node_embeddings[src]
-            dst_emb = node_embeddings[dst]
+        # adj_matrix = torch.zeros(node_embeddings.shape[0], node_embeddings.shape[0])
+        # mask = ~torch.eye(node_embeddings.shape[0], dtype=torch.bool)
+        # for i in range(edge_index.shape[1]):
+            # src = edge_index[0][i]
+            # dst = edge_index[1][i]
+            # src_emb = node_embeddings[src]
+            # dst_emb = node_embeddings[dst]
 
-            pair_embeddings = torch.cat([src_emb, dst_emb], dim=-1)
-            link_probs = self.link_predictor(pair_embeddings)
-            adj_matrix[src][dst] = (link_probs >= 0.5).long()
-        adj_matrix = adj_matrix[mask]
-        print(adj_matrix)
-        return adj_matrix
+            # pair_embeddings = torch.cat([src_emb, dst_emb], dim=-1)
+            # link_probs = self.link_predictor(pair_embeddings)
+            # adj_matrix[src][dst] = (link_probs >= 0.5).long()
+        # adj_matrix = adj_matrix[mask]
+        # print(adj_matrix)
+        # return adj_matrix
+
+
+        # candidate_edge_index: tensor di shape [2, num_candidate_edges]
+        src = edge_index[0]  # Source nodes
+        dst = edge_index[1]  # Destination nodes
+
+        pair_embeddings = torch.cat([node_embeddings[src], node_embeddings[dst]], dim=-1)
+        link_probs = self.link_predictor(pair_embeddings)  # shape [num_candidate_edges]
+        return link_probs
 
     def training_step(self, batch, batch_idx):
-        print(batch)
-
-        link_probs = self(batch)
-        link_labels = batch["edu", "to", "edu"].link_labels
+        # print(batch)
+        link_probs = self(batch)  # Model predictions
+        link_labels = batch["edu", "to", "edu"].link_labels.long()
         link_loss = self.link_pl(link_probs, link_labels)
 
         relation_loss = 0
@@ -107,5 +118,5 @@ if __name__ == "__main__":
     data_module.setup(stage="fit")
     train_loader = data_module.train_dataloader()
 
-    trainer = pl.Trainer()
+    trainer = pl.Trainer(max_epochs=10, log_every_n_steps=10)
     trainer.fit(model=model, train_dataloaders=train_loader)
